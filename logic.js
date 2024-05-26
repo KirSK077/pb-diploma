3b27d4dc8f2cac2a1d6c8029e5ab8d35f3721225
KirSK077 <121286716+KirSK077@users.noreply.github.com>
2024-05-26 21:19:51 +0500

    Обновлен файл logic.js

--- logic.js
+++ logic.js
@@ -14,20 +14,18 @@ function startGame() {
   counterClicks = 0;
 }
 
-function checkWin(board, player) {
-  for (let i = 0; i < 3; i++) {
-    if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
-      showWinner(activePlayer);
-    } 
-    else if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
-      showWinner(activePlayer);
-    }
-    else if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
-      showWinner(activePlayer);
-    }
-    else if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
-      showWinner(activePlayer);
-    }
+function checkWin(board, player, row, col) {
+  if (board[row][0] === player && board[row][1] === player && board[row][2] === player) {
+    showWinner(activePlayer);
+  } 
+  else if (board[0][col] === player && board[1][col] === player && board[2][col] === player) {
+    showWinner(activePlayer);
+  }
+  else if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+    showWinner(activePlayer);
+  }
+  else if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+    showWinner(activePlayer);
   }
 }
 
@@ -36,5 +34,5 @@ function click(row, col) {
   counterClicks++;
   board[row][col] = players[activePlayer];
   renderBoard(board);
-  checkWin(board, players[activePlayer]);
+  checkWin(board, players[activePlayer], row, col);
 }
